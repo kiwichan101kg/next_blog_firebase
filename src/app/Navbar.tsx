ba5a424fbc02931c139416f0bb24db0b4b440039
kawajiharuka <kjn.hk10001@gmail.com>
2023-07-30 23:08:37 +0900

    ナビゲーションバー作成

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,12 +1,39 @@
 import Link from "next/link";
 import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href={"/"}>ホーム</Link>
-      <Link href={"/posts"}>記事投稿</Link>
-      <Link href={"/login"}>ログイン</Link>
+    <nav className="bg-teal-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-semibold text-lg">
+          <Link href={"/"}>
+            {" "}
+            <MenuIcon className="mx-4 " />
+            Blog
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Link
+            href={"/posts"}
+            className="text-white hover:text-white hover:bg-teal-400 duration-200 scale-95  rounded-full px-3 py-2"
+          >
+            Post
+          </Link>
+          <Link
+            href={"/contact"}
+            className="text-white hover:text-white hover:bg-teal-400 duration-200 scale-95  rounded-full px-3 py-2"
+          >
+            Contact
+          </Link>
+          <Link
+            href={"/login"}
+            className="text-white hover:text-white hover:bg-teal-400 duration-200 scale-95 border rounded-full px-4 py-2"
+          >
+            login
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <h1>ログインで始める</h1>
+      <button className="px-2 py-1 bg-white text-blue-500 border border-blue-500 font-semibold rounded hover:bg-blue-100 duration-200 hover:scale-95">
+        Googleでログイン
+      </button>
+    </div>
+  );
 };
 
 export default page;

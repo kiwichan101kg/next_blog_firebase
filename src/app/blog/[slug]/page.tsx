52c65c80cf4ee587132e7960b480ea6594c4b1b6
kawajiharuka <kjn.hk10001@gmail.com>
2023-09-07 00:10:50 +0900

    ページネーションprev,Next

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { getPostBySlug } from "@/app/api";
+import { getPostBySlug, getPosts } from "@/app/api";
 import Container from "@/app/components/Container";
 import FlexBox from "@/app/components/FlexBox";
 import Preview from "@/app/posts/Preview";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import PrevNextPagination from "./PrevNextPagination";
 
 type PostSlug = {
   params: { slug: string };
@@ -19,15 +21,41 @@ const page = ({ params: { slug } }: PostSlug) => {
     setPosts();
   }, []);
 
-  console.log(postContent);
+  const [allPosts, setAllPosts] = useState<any[]>([]);
+  useEffect(() => {
+    const setPosts = async () => {
+      const data = await getPosts();
+      setAllPosts(data);
+    };
+    setPosts();
+  }, []);
+
+  const [prevPost, nextPost] = prevNextPost(allPosts, slug);
 
   return (
     <Container>
       <FlexBox>
         <Preview title={postContent?.title} body={postContent?.body}></Preview>
+        <PrevNextPagination prevPost={prevPost} nextPost={nextPost} />
       </FlexBox>
     </Container>
   );
+};
+
+const prevNextPost = (allPosts: any[], currentSlug: string) => {
+  const numberOfPosts = allPosts.length;
+
+  // 取得したallpostsの配列の中から、現在のslugと一致するものの配列のindexを返却する
+  const index = allPosts.findIndex((item) => item.post_id === currentSlug);
+  console.log(index);
+
+  // 現在の投稿が最初でなければ前の投稿を取得
+  const prevPost = index === 0 ? undefined : allPosts[index - 1];
+  // 現在の投稿が最後でなければ次の投稿を取得
+  const nextPost =
+    index + 1 === numberOfPosts ? undefined : allPosts[index + 1];
+
+  return [prevPost, nextPost];
 };
 
 export default page;
--- src/app/posts/Preview.tsx
+++ src/app/posts/Preview.tsx
@@ -3,9 +3,17 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 
-const Preview = ({ title, body }: { title: string; body: string }) => {
+const Preview = ({
+  title,
+  body,
+  full,
+}: {
+  title: string;
+  body: string;
+  full?: boolean;
+}) => {
   return (
-    <>
+    <div className={`flex flex-col gap-10 my-8 ${full ? "min-h-screen" : ""}`}>
       <h1 className="font-bold text-4xl">{title}</h1>
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
@@ -14,7 +22,7 @@ const Preview = ({ title, body }: { title: string; body: string }) => {
       >
         {body}
       </ReactMarkdown>
-    </>
+    </div>
   );
 };
 
